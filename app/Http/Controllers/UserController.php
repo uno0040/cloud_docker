640e72768ce2eaa5cf15bd7ea2e5afe32ed0faed
uno0040 <montanhauno0040@gmail.com>
2024-09-12 21:32:05 -0300

    rota de registro

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Recebe usuário e senha, e insere os mesmos na tabela de usuários. Se o usuário já exisir, retorna 409.
+     * @param \Illuminate\Http\Request $request
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function createUser(Request $request) {
+        $validated = $request->validate([
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|max:255'
+        ]);
+
+        // Verifica se o usuário já existe
+        $user = User::where('username', '=', $validated['username'])->first();
+
+        if ($user === null) {
+            // Hash da senha antes de criar o usuário
+            // $validated['password'] = Hash::make($validated['password']);
+
+            // Cria o usuário
+            User::create($validated);
+
+            return response()->json(['message' => 'Usuário criado com sucesso.'], 200);
+        } else {
+            return response()->json(['message' => 'Usuário já existente. Faça login.'], 409);
+        }
+    }
+    // public function updatePassword(Request $request) {
+    //     $validated = $request->validate([
+    //         'username' => 'required|string|max:255',
+    //         'password' => 'required|string|max:255'
+    //     ]);
+
+    //     // Verifica se o usuário já existe
+    //     $user = User::where('username', '=', $validated['username'])->first();
+
+
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('login');
 })->name('login');
+
+Route::put('/cadastro', [UserController::class,'createUser']);
 
 Route::get('/cadastro', function () {
     return view('cadastro');
